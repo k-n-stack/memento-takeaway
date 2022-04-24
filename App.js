cdb8c025500f299b2a39cbcd559c9c744d37285b
unknown <brjopr@gmail.com>
2022-04-24 10:39:39 +0200

    initial commit and expo tests

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style={styles.statusBar} translucent={false} backgroundColor="white"/>
+      <LinearGradient
+        colors={['#3261cf', '#694896']}
+        start={[0,1]}
+        end={[1,0]}
+        style={styles.background}
+      >
+        <View style={styles.contentContainer}>
+          <Text style={styles.text}>Sign in with Facebook</Text>
+        </View>
+      </LinearGradient>
     </View>
   );
 }
@@ -17,4 +27,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  contentContainer: {
+    backgroundColor: 'transparent',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+  },
+  statusBar: {
+    backgroundColor: 'red',
+  }
 });
